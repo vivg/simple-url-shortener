35e91f7dd2aaf1e9daec0794c9789211e70089fa
Vivek Gupta <mail@vivekgupta.com>
2016-11-12 07:47:27 +0530

    fixed routes
    Signed-off-by: Vivek Gupta <mail@vivekgupta.com>

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/shorten', 'ApiController@shorten');
 
-
-Route::group(['prefix' => 'api'], function () {
-
-    Route::post('/shorten', 'ApiController@shorten');
-
-    Route::get('/urls', 'ApiController@listUrls');
-
-});
+Route::get('/urls', 'ApiController@listUrls');
