b75061657cf3345df6d432530a3f111eb302eb46
Vivek Gupta <mail@vivekgupta.com>
2016-11-12 07:36:35 +0530

    Added api and redirect controllers
    Signed-off-by: Vivek Gupta <mail@vivekgupta.com>

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Requests\ShortenUrlRequest;
+use App\Models\ShortUrl;
+use App\Models\DeviceUrl;
+
+class ApiController extends Controller
+{
+    /**
+     * @var ShortUrl
+     */
+    protected $shortUrlModel;
+
+    /**
+     * ApiController constructor.
+     * @param ShortUrl $shortUrlModel
+     */
+    public function __construct(ShortUrl $shortUrlModel)
+    {
+        $this->shortUrlModel = $shortUrlModel;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function listUrls()
+    {
+        $urls = $this->shortUrlModel->with('urls')->paginate(10);
+
+        return $urls;
+    }
+
+
+    /**
+     * @param ShortenUrlRequest $request
+     * @return ShortUrl
+     */
+    public function shorten(ShortenUrlRequest $request)
+    {
+        //generate hash
+        $hash = $this->uniqueHash();
+        $shortUrl = new ShortUrl();
+
+        DB::beginTransaction();
+        try {
+            $shortUrl->short_code = $hash;
+            $shortUrl->save();
+
+            $deviceUrls = [];
+
+            if($request->exists('url')) {
+                $deviceUrl = new DeviceUrl();
+                $deviceUrl->long_url = $request->get('url');
+                $deviceUrl->device_type = 'desktop';
+                $deviceUrls[] = $deviceUrl;
+            }
+
+            if($request->exists('urls')) {
+
+                $deviceUrls = [];
+                $urls = $request->get('urls');
+
+                foreach ($urls as $deviceType => $url) {
+                    $deviceUrl = new DeviceUrl();
+                    $deviceUrl->long_url = $url;
+                    $deviceUrl->device_type = $deviceType;
+                    $deviceUrls[] = $deviceUrl;
+                }
+            }
+            if(empty($deviceUrl)) {
+                throw new Exception;
+            }
+            $shortUrl->urls()->saveMany($deviceUrls);
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            abort('500', 'Error in generating short url. Please try again later');
+        }
+
+        return $shortUrl;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    protected function uniqueHash()
+    {
+        //generate random hash
+        $hash = random_hash();
+
+        //check if hash exists in db, generate again if it does
+        $hashExists = $this->shortUrlModel->where('short_code', $hash)->first();
+
+        return $hashExists ? $this->uniqueHash(): $hash;
+    }
+
+}
---
+++ app/Http/Controllers/RedirectController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShortUrl;
+use Jenssegers\Agent\Agent;
+use Redirect;
+use Exception;
+
+
+class RedirectController extends Controller
+{
+    public $shortUrlModel;
+    public $agent;
+
+    public function __construct(
+        ShortUrl $shortUrlModel,
+        Agent $agent
+    ) {
+        $this->shortUrlModel = $shortUrlModel;
+        $this->agent = $agent;
+    }
+
+    public function redirect($shortCode)
+    {
+        $shortUrl = $this->shortUrlModel->where('short_code', $shortCode)->with('urls')->first();
+
+        //check whether the shortcode exists , if not throw 404
+        if(!$shortUrl) {
+            abort(404);
+        }
+
+        $deviceType = $this->deviceType();
+
+        //check the agent and see if corresponding url type exists, if not default to desktop url
+        $urls = $shortUrl->urls->keyBy('device_type')->all();
+
+        $redirectUrl = $urls['desktop'];
+
+        if(key_exists($deviceType, $urls)) {
+            $redirectUrl = $urls[$deviceType];
+        }
+
+        //increment redirect count for the device type, throw a 500 in case of db error
+        try {
+            $redirectUrl->redirect_count +=1;
+            $redirectUrl->save();
+        } catch (Exception $e) {
+            //we should also log here to investigate later.
+            abort(500, 'Something went wrong. Please try again later.');
+        }
+
+
+        //redirect to the appropriate url with a 301 status code
+        return Redirect::to($redirectUrl->long_url, 301);
+
+    }
+
+    protected function deviceType()
+    {
+        if($this->agent->isDesktop()) {
+            return 'desktop';
+        }
+
+        if($this->agent->isMobile()) {
+            return 'mobile';
+        }
+
+        if($this->agent->isTablet()) {
+            return 'tablet';
+        }
+
+        return 'desktop';
+    }
+
+
+
+}
